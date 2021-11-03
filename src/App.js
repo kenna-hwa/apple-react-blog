5e6385db88319633bf984f15a2074b99431d446d
Kenna <kenna.hwa@gmail.com>
2021-11-03 23:25:18 +0900

    중요한 데이터는 변수말고 리액트 state로 만들랬죠

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import  React, { useState } from "react";
 import fff from './f.jpg';
 import './App.css';
 
 function App() {
-  let posts = "강남 고기 맛집";
-  function fun(){
-    return 100;
-  }
+  
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '홍대 독립 서점']);
+  
+  let [내용, 내용변경] = useState(["남자코트는 여기가 짱", "강남 우동 맛있어요", "재미있는 책이 있어요"]);
+  
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <h4>{ fun() }</h4>
-      <img src={ fff } style={ { width : '400px', boxShadow : '0 0 10px'}}></img>
+      <div className="list">
+        <h4>{ 글제목[0] }</h4>
+        <p>{ 내용[0] }</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>{ 내용[1] }</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>{ 내용[2] }</p>
+      </div>
+
     </div>
   );
 }
